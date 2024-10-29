85cc6f843e33b070a42eb024b7b0161e1eae0fdb
Subhamay Paul <subhomaypaul@gmail.com>
2024-10-29 23:52:17 +0530

    Latest release

--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import FormSection from '../_components/FormSection'
 import OutputSection from '../_components/OutputSection'
 import { TEMPLATE } from '../../_components/TemplateListSection'
@@ -7,6 +7,7 @@ import Templates from '@/app/(data)/Templates'
 import Link from 'next/link'
 import { ArrowLeft } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { chatSession } from '@/utils/AiModel'
 
 interface PROPS{
     params:{
@@ -18,8 +19,19 @@ function CreateNewContent(props:PROPS) {
 
     const selectedTemplate:TEMPLATE|undefined=Templates?.find((item)=>item.slug==props.params['template-slug']);
 
-    const generateAIContent = (formData:any) => {
+    const [loading,setLoading] = useState(false);
+    const [aiOutput, setAiOutput] = useState<string>('');
 
+    const generateAIContent = async (formData:any) => {
+        setLoading(true);
+        const SelectedPrompt=selectedTemplate?.aiPrompt;
+        const FinalAIPrompt=JSON.stringify(formData)+", "+SelectedPrompt;
+
+        const result = await chatSession.sendMessage(FinalAIPrompt);
+
+        console.log(result.response.text());
+        setAiOutput(result?.response.text());
+        setLoading(false);
     }
 
   return (
@@ -29,10 +41,11 @@ function CreateNewContent(props:PROPS) {
         </Link>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-5 py-5'>
             {/* FormSection */}
-            <FormSection selectedTemplate={selectedTemplate} userFormInput={(v:any)=>generateAIContent(v)}/>
+            <FormSection selectedTemplate={selectedTemplate} userFormInput={(v:any)=>generateAIContent(v)}
+            loading={loading} />
             {/* OutputSection */}
             <div className='col-span-2'>
-                <OutputSection />
+                <OutputSection aiOutput={aiOutput}/>
             </div>
         </div>
     </div>
--- app/dashboard/content/_components/OutputSection.tsx
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import '@toast-ui/editor/dist/toastui-editor.css';
 import { Editor } from '@toast-ui/react-editor';
 import { Button } from '@/components/ui/button';
 import { Copy } from 'lucide-react';
 
-function OutputSection() {
+interface props{
+    aiOutput:string;
+}
+
+function OutputSection({aiOutput}:props) {
+
+    const editorRef:any = useRef();
+
+    useEffect(()=>{
+        const editorInstance=editorRef.current.getInstance();
+        editorInstance.setMarkdown(aiOutput);
+    },[aiOutput])
+
   return (
-    <div className='bg-white shadow-lg border'>
-        <div className='flex justify-between items-center'>
-            <h2>Your Result</h2>
-            <Button><Copy/>Copy</Button>
+    <div className='bg-white shadow-lg border rounded-lg'>
+        <div className='flex justify-between items-center p-5'>
+            <h2 className='font-medium text-lg'>Your Result</h2>
+            <Button className='flex gap-2'><Copy className='w-4 h-4'/>Copy</Button>
         </div>
         <Editor
-            initialValue="hello react editor world!"
-            previewStyle="vertical"
+        ref={editorRef}
+            initialValue="Your result will appear here"
+            // previewStyle="vertical"
             height="600px"
-            initialEditType="markdown"
+            initialEditType="wysiwyg"
             useCommandShortcut={true}
+            onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
         />
     </div>
   )
